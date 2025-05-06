a44b062e4ceecb00945342e3d475932bcf67ae87
sukumar38140 <sukumar38140@gmail.com>
2025-05-06 10:25:51 +0530

    success

---
+++ main.js
@@ -0,0 +1,15 @@
+var typed = new Typed('.text', {
+    strings: ["Fronted Developer.", "Web Developer." ,"Mobile Developer." ,"YouTuber."] ,
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop:true
+  });
+
+  var typed = new Typed('.main', {
+    strings: ["Kumar Saatharla.", ] ,
+    typeSpeed: 100,
+    backSpeed: 150,
+    backDelay: 180,
+    loop:true
+  });
